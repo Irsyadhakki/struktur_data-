80bc2d70166a6f7c970527eec619ed465cd1a259
Irsyad Hakki <145946160+Irsyadhakki@users.noreply.github.com>
2024-06-04 15:14:36 +0800

    belajar percabangan

---
+++ meet8.py
@@ -0,0 +1,29 @@
+# if kondisi : aksi
+#belajar percabangan
+
+nama = input('masukan : ')
+
+# if ,elif ,else
+
+if nama == 'edy' :
+    print("dosen")
+elif nama == 'hakki':
+    print("mahasiswa")     
+else : 
+    print('gak kenal')    
+    
+print('program selesai')
+
+# if 
+
+#if nama == 'edy' :
+#    print("dosen")
+    
+#print('program selesai')
+
+# if, else
+
+#if nama == 'edy' :
+#    print("dosen")    
+#else : 
+#    print('gak kenal')
