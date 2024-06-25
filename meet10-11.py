56dff3defc6b6b1c47275f16d0287707c5a7c009
Irsyad Hakki <145946160+Irsyadhakki@users.noreply.github.com>
2024-06-25 18:08:04 +0800

    belajar perulangan

---
+++ meet10-11.py
@@ -0,0 +1,39 @@
+#belajar perulangan (looping)
+
+#bil = 1
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+
+#range adalah 
+
+bil = [0,1,2,3,4]
+
+
+for i in bil :
+    print(f"nilai i sekarang ==> {i}" )
+    print('akhir program 1')
+    
+    
+#menggunakan range
+bil2 = range(10)
+for i in bil2 :
+    print(f"nilai i sekarang ==> {i}")
+    print("akhir program 1")
+    
+ 
+    
+    
+    
+        
+    
+    
+    
+    
+    
+    
